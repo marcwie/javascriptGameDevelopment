0b9450933a21849ee6e824cdf87d8cff176e5e28
Marc Wiedermann <marcwiedermann@posteo.de>
2022-08-07 16:35:03 +0200

    refactor code to get rid of x2 variable

--- parallax_backgrounds/script.js
+++ parallax_backgrounds/script.js
@@ -17,60 +17,54 @@ backgroundLayer4.src = 'backgroundLayers/layer-4.png'
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'backgroundLayers/layer-5.png'
 
-const slider = document.getElementById('slider');
-slider.value = gameSpeed;
-const gameSpeedDisplay = document.getElementById('gameSpeedValue')
-gameSpeedDisplay.innerHTML = gameSpeed;
-
-slider.addEventListener('change', (e) => {
-    gameSpeed = e.target.value;
-    gameSpeedDisplay.innerHTML = e.target.value;
-})
-
 class Layer {
     constructor(image, speedModifier) {
         this.x = 0;
         this.y = 0;
         this.width = 2400;
         this.height = 700;
-        this.x2 = this.width;
         this.image = image;
         this.speedModifier = speedModifier;
         this.speed = gameSpeed * this.speedModifier;
     }
     update(){
         this.speed = gameSpeed * this.speedModifier;
-
-        if (this.x < -this.width) {
-            this.x = this.width + this.x2 - this.speed;
-        }
-        if (this.x2 < -this.width) {
-            this.x2 = this.width + this.x - this.speed;
-        }   
-
-        this.x = Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x2 - this.speed);
+        if (this.x < -this.width) this.x = 0; 
+        this.x = this.x - this.speed;
     }
     draw(){
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
-        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height)
     }
 }
 
-const layer5 = new Layer (backgroundLayer5, 1.0);
-const layer4 = new Layer (backgroundLayer4, 0.8);
-const layer3 = new Layer (backgroundLayer3, 0.6)
-const layer2 = new Layer (backgroundLayer2, 0.4)
-const layer1 = new Layer (backgroundLayer1, 0.2)
-const layers = [layer1, layer2, layer3, layer4, layer5]
+window.addEventListener('load', function() {
+    const slider = document.getElementById('slider');
+    slider.value = gameSpeed;
+    const gameSpeedDisplay = document.getElementById('gameSpeedValue')
+    gameSpeedDisplay.innerHTML = gameSpeed;
 
-function animate () {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-    layers.forEach(e => {
-        e.update();
-        e.draw();
-    });    
-    requestAnimationFrame(animate)
-}
+    slider.addEventListener('change', (e) => {
+        gameSpeed = e.target.value;
+        gameSpeedDisplay.innerHTML = e.target.value;
+    })
 
-animate()
+    const layer5 = new Layer (backgroundLayer5, 1.0);
+    const layer4 = new Layer (backgroundLayer4, 0.8);
+    const layer3 = new Layer (backgroundLayer3, 0.6)
+    const layer2 = new Layer (backgroundLayer2, 0.4)
+    const layer1 = new Layer (backgroundLayer1, 0.2)
+    const layers = [layer1, layer2, layer3, layer4, layer5]
+
+    function animate () {
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+        layers.forEach(e => {
+            e.update();
+            e.draw();
+        });    
+        requestAnimationFrame(animate)
+    }
+
+    animate()
+
+})
